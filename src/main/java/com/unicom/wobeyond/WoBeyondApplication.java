06f07df95e18c00facb535cccfcc434d7a21baa3
beyond_champion@163.com <930912wc_git>
2018-10-16 22:07:07 +0800

    初始化wo-beyond项目...

---
+++ src/main/java/com/unicom/wobeyond/WoBeyondApplication.java
@@ -0,0 +1,12 @@
+package com.unicom.wobeyond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoBeyondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoBeyondApplication.class, args);
+    }
+}
---
+++ src/test/java/com/unicom/wobeyond/WoBeyondApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unicom.wobeyond;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WoBeyondApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
